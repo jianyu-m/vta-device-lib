3adf3a58760113472a5f6afa7e985b27a1127145
jianyu-m <jianyu@hku.hk>
2022-01-01 01:22:44 +0800

    sit

--- sit/sit.cpp
+++ sit/sit.cpp
@@ -16,24 +16,121 @@ void *sit_memory;
 #define TAG_KB 4
 #define SIT_KB 4
 
+#define CACHE_SIZE 64
+
+typedef struct {
+    union {
+        struct {
+            int main;
+            int tag;
+            int sit;
+        };
+        int data[3];
+    };
+
+} memory_size_t;
+
+typedef struct {
+    union {
+        struct {
+            cacheline_t *memory;
+            raw_tag_t *tags;
+            counter_t *sit;
+            uint8_t *raw_memory;
+        };
+        uint8_t* data[4];
+    };
+
+} memory_t;
+
+data_t root_cnt = {0, 0};
+
+void aes128_dec_cacheline(__m128i key_schedule[20], uint8_t *cipher, uint8_t *plaintext) {
+    aes128_dec(key_schedule, (uint8_t *)cipher + 0 , (uint8_t *)plaintext + 0 );
+    aes128_dec(key_schedule, (uint8_t *)cipher + 16, (uint8_t *)plaintext + 16);
+    aes128_dec(key_schedule, (uint8_t *)cipher + 32, (uint8_t *)plaintext + 32);
+    aes128_dec(key_schedule, (uint8_t *)cipher + 48, (uint8_t *)plaintext + 48);
+}
+
+void aes128_enc_cacheline(__m128i key_schedule[20], uint8_t *plaintext, uint8_t *cipher) {
+    aes128_enc(key_schedule, (uint8_t *)plaintext + 0 , (uint8_t *)cipher + 0 );
+    aes128_enc(key_schedule, (uint8_t *)plaintext + 16, (uint8_t *)cipher + 16);
+    aes128_enc(key_schedule, (uint8_t *)plaintext + 32, (uint8_t *)cipher + 32);
+    aes128_enc(key_schedule, (uint8_t *)plaintext + 48, (uint8_t *)cipher + 48);
+}
+
+inline int compare_tag(raw_tag_t a, raw_tag_t b) {
+    return ((data_t *)a)->c == ((data_t *)b)->c;
+}
+
+inline void copy_tag(raw_tag_t dst, raw_tag_t src) {
+    memcpy(dst, src, sizeof(raw_tag_t));
+}
+
+void read_cacheline(memory_t *memory, int index, uint8_t* plaintext, __m128i key_schedule[20]) {
+    // aes128_enc(key_schedule, (uint8_t*) main_memory + index / 64, (uint8_t*) main_memory + 16);
+    raw_tag_t tag;
+    data_t *counter;
+
+    aes128_dec_cacheline(key_schedule, (uint8_t *)(memory->raw_memory + index), plaintext);
+
+    int cl_index = index / 64;
+    int cnt_div = cl_index / 8;
+    int cnt_mod = cl_index % 8;
+
+    // compare the first level
+    counter = (data_t *)(memory->sit[cnt_div].counters[cnt_mod]);
+    vmac64(key_schedule, (uint8_t *)(memory->raw_memory + index), tag, counter->c);
+    compare_tag(memory->tags[cl_index], tag);
+    // memcmp(tag, memory[1] + cl_index * 8, 7);
+
+    // compare the second level
+    raw_tag_t tag2;
+    vmac56(key_schedule, (uint8_t*)(&memory->sit[cnt_div]), tag2, root_cnt.c);
+    compare_tag(memory->sit[cnt_div].tag, tag2);
+}
+
+void write_cacheline(memory_t *memory, int index, uint8_t* plaintext, __m128i key_schedule[20]) {
+    // aes128_enc(key_schedule, (uint8_t*) main_memory + index / 64, (uint8_t*) main_memory + 16);
+    uint8_t tag[16];
+    data_t *counter;
+
+    aes128_enc_cacheline(key_schedule, plaintext, (uint8_t *)(memory->raw_memory + index));
+
+    int cl_index = index / 64;
+    int cnt_div = cl_index / 8;
+    int cnt_mod = cl_index % 8;
+
+    // compare the first level
+    counter = (data_t *)(memory->sit[cnt_div].counters[cnt_mod]);
+    vmac64(key_schedule, (uint8_t *)(memory->raw_memory + index), tag, counter->c);
+    copy_tag(memory->tags[cl_index], tag);
+
+    // compare the second level
+    raw_tag_t tag2;
+    root_cnt.c += 1;
+    vmac56(key_schedule, (uint8_t*)(&memory->sit[cnt_div]), tag2, root_cnt.c);
+    copy_tag(memory->sit[cnt_div].tag, tag2);
+}
+
 int main() {
-    int main_bytes = MAIN_KB * 1024;
-    int tag_bytes = TAG_KB * 1024; 
-    int sit_bytes = SIT_KB * 1024;
-    main_memory = mmap(0, main_bytes, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    tag_memory = mmap(0, tag_bytes, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    sit_memory = mmap(0, sit_bytes, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    
+    memory_size_t sizes = {4096, 4096, 4096};
+    memory_t memory = {NULL, NULL, NULL};
+    for (int i  = 0; i < 3;i++) {
+        memory.data[i] = (uint8_t*) mmap(0, sizes.data[i], PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    }
+    memory.data[3] = memory.data[0];
+
     uint8_t enc_key[]    = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
     __m128i key_schedule[20];
     
     aes128_load_key(enc_key,key_schedule);
-    aes128_enc(key_schedule, (uint8_t*) main_memory, (uint8_t*) main_memory + 16);
-    aes128_dec(key_schedule,(uint8_t*) main_memory + 16,(uint8_t*) main_memory);
-
-    vmac64(key_schedule, (uint8_t *)main_memory + 16, (uint8_t *)tag_memory, 0);
-
-    std::cout << *(int*)(main_memory + 16) << "\n";
-    std::cout << *(int*)main_memory <<  "\n";
-    std::cout << *(int*)tag_memory <<  "\n";
+    
+    // write_cacheline()
+    uint8_t content[64];
+    uint8_t content2[64];
+    content[0] = 1;
+    write_cacheline(&memory, 0, content, key_schedule);
+    read_cacheline(&memory, 0, content2, key_schedule);
+    printf("c %d\n", content2[0]);
 }
--- sit/vmac.h
+++ sit/vmac.h
@@ -3,11 +3,31 @@
 
 #include "aes_ni.h"
 
-uint64_t vhash(uint8_t *message) {
-    return *(uint64_t *)(message);
+typedef uint8_t cacheline_t[64];
+typedef uint8_t raw_counter_t[7];
+typedef uint8_t raw_tag_t[7];
+
+typedef struct {
+    uint64_t c : 56;
+    uint64_t pad : 8;
+} data_t;
+
+typedef struct {
+    raw_counter_t counters[8];
+    raw_tag_t tag;
+    uint8_t pad;
+} counter_t;
+
+uint64_t vhash64(uint8_t *message) {
+    return ((data_t *)(message))->c;
 }
 
-void vmac64(__m128i *key_schedule, uint8_t *message, uint8_t *tag, uint64_t counter) {
+uint64_t vhash56(uint8_t *message) {
+    counter_t *c = (counter_t*) message;
+    return ((data_t *)(c->counters))->c;
+}
+
+inline void vmac56(__m128i *key_schedule, uint8_t *message, raw_tag_t tag, uint64_t counter) {
     // vmac computation
     // vmac(m, nonce) = Hash(M) + AES-128(Nonce)
     uint8_t nonce[16];
@@ -15,7 +35,20 @@ void vmac64(__m128i *key_schedule, uint8_t *message, uint8_t *tag, uint64_t coun
     memset(nonce, 0, 16);
     memcpy(nonce, &counter, sizeof(counter));
     aes128_enc(key_schedule, nonce, encrypted_nonce);
-    uint64_t h = vhash(message);
+    uint64_t h = vhash56(message);
     uint64_t r = h + *(uint64_t*)(encrypted_nonce);
-    memcpy(tag, &r, sizeof(r));
+    memcpy(tag, &r, sizeof(raw_tag_t));
+}
+
+inline void vmac64(__m128i *key_schedule, uint8_t *message, raw_tag_t tag, uint64_t counter) {
+    // vmac computation
+    // vmac(m, nonce) = Hash(M) + AES-128(Nonce)
+    uint8_t nonce[16];
+    uint8_t encrypted_nonce[16];
+    memset(nonce, 0, 16);
+    memcpy(nonce, &counter, sizeof(counter));
+    aes128_enc(key_schedule, nonce, encrypted_nonce);
+    uint64_t h = vhash64(message);
+    uint64_t r = h + *(uint64_t*)(encrypted_nonce);
+    memcpy(tag, &r, sizeof(raw_tag_t));
 }
